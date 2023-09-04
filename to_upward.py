a68da00dd1d117b1a29344162e1529cd7be9e177
LeeDanHee <ldh040506@gmail.com>
2023-09-04 14:42:10 +0900

    몬테카를로, module구성 완료

---
+++ to_upward.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+from tqdm.auto import tqdm
+import random
+import os
+from pykrx import stock
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+import warnings
+warnings.filterwarnings('ignore')
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import sympy
+
+def pad_str(str_list, target_len):
+
+  padded_str_list = []
+  for str in str_list:
+    if len(str) < target_len:
+      padded_str = "0" * (target_len - len(str)) + str
+    else:
+      padded_str = str
+    padded_str_list.append(padded_str)
+  return padded_str_list
+
+def get_close(data,stocks,start,end):
+    tmp = pd.DataFrame()
+    for n in stocks:
+        tmp[n] = stock.get_market_ohlcv(start, end, data[data['Name'] == n]['Code'])['종가']
+    return tmp
+
+def get_portfolio(stocks,annual_ret,annual_cov):
+  port_ret = []
+  port_risk = []
+  port_weights = []
+  shape_ratio = []
+
+  for i in range(30000):
+
+      weights = np.random.random(len(stocks))
+      weights /= np.sum(weights)
+
+
+      returns = np.dot(weights, annual_ret)
+
+
+      risk = np.sqrt(np.dot(weights.T, np.dot(annual_cov, weights)))
+
+      port_ret.append(returns)
+      port_risk.append(risk)
+      port_weights.append(weights)
+      shape_ratio.append(returns/risk)
+
+  portfolio = {'Returns' : port_ret, 'Risk' : port_risk, 'Shape' : shape_ratio}
+  for j, s in enumerate(stocks):
+      portfolio[s] = [weight[j] for weight in port_weights]
+
+  df = pd.DataFrame(portfolio)
+
+  max_shape = df.loc[df['Shape'] == df['Shape'].max()]
+  min_risk = df.loc[df['Risk'] == df['Risk'].min()]
+  tmp2 = df.groupby('Risk')[['Returns']].max().reset_index()
+
+  best_ret = tmp2.loc[0,'Returns']
+  for i in range(tmp2.shape[0]):
+    if tmp2.loc[i,'Returns']<best_ret:
+      tmp2.drop(index=i,inplace=True)
+    else:
+      best_ret = tmp2.loc[i,'Returns']
+  return max_shape,min_risk,tmp2,df
+
+def show_CAPM(df, tmp2, max_shape, min_risk, rf=0.035):
+    df.plot.scatter(x='Risk', y='Returns', c='Shape', cmap='viridis', edgecolors='k', figsize=(10,8), grid=True)
+    plt.plot(tmp2['Risk'], tmp2['Returns'], label='Efficient Frontier', linewidth=5,color='red')
+    plt.scatter(max_shape['Risk'], max_shape['Returns'], label='Max_Shape', marker='*',s=500)
+    plt.scatter(min_risk['Risk'], min_risk['Returns'], label='Min_risk', marker='*', s=500)
+    plt.plot([0, max_shape['Risk'].iloc[0], 0.5], [rf, max_shape['Returns'].iloc[0], (max_shape['Returns'].iloc[0] - rf) / max_shape['Risk'].iloc[0] * 0.5 + rf], label='New EF', linewidth=2,color='green')
+    plt.xlabel('Risk')
+    plt.ylabel('Expected Return')
+    plt.title('Efficient Frontier Graph')
+    plt.legend()
+    plt.show()
+
+def show_portfolio(max_shape):
+
+    exp_ret = float(input("원하는 기대수익률은 얼마인가?"))/100
+    w = sympy.Symbol('w')
+
+    equation = w*0.02 + (1-w)*max_shape['Returns'].values[0] - exp_ret
+
+    solution = sympy.solve(equation, w)
+    solution = float(solution[0])
+    if solution < 0 :
+        print(f"차입 비중 : {-solution}")
+        print(f"이 경우 Risk : {(1-solution)*max_shape['Risk'].iloc[0]}")
+    else : 
+        print(f"채권의 비중 : {solution}")
+        print(f"이 경우 Risk : {(1-solution)*max_shape['Risk'].iloc[0]}")
+        
+    if solution >= 0:
+
+        fig = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]],subplot_titles=("<b>포트폴리오", f"<b>기대수익을 위한 포트폴리오<br><sup>자기자본의 {solution*100:0.4}%만큼 채권투자</sup>"))
+
+
+        fig.add_trace(go.Pie(
+            values=list(max_shape.values[0][-5:]),
+            labels=list(max_shape.columns[-5:]),
+            domain=dict(x=[0, 0.5]),
+            name="기존 포트폴리오"),
+            row=1, col=1)
+
+        fig.add_trace(go.Pie(
+            values=list(max_shape.values[0][-5:]* (1-float(solution)))+[float(solution)] ,
+            labels=list(max_shape.columns[-5:]) + ['채권'],
+            domain=dict(x=[0.5, 1.0]),
+            name="기대수익 포트폴리오"),
+            row=1, col=2)
+
+        fig.show()
+
+    else:
+        fig = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]],subplot_titles=("<b>포트폴리오", f"<b>투자금 비중</b><br><sup>자기자본의 {-solution*100:0.4}%만큼 차입</sup>"))
+
+
+        fig.add_trace(go.Pie(
+            values=list(max_shape.values[0][-5:]),
+            labels=list(max_shape.columns[-5:]),
+            domain=dict(x=[0, 0.5])),
+            row=1, col=1)
+
+        fig.add_trace(go.Pie(
+            values=[1/(1-solution),1-(1/(1-solution))] ,
+            labels=['자기자본','차입금'],
+            domain=dict(x=[0.5, 1.0])),
+            row=1, col=2)
+
+        fig.show()
+
+def geometric_brownian_motion(tmp,S0, T=100, dt=1/100):
+    """
+    S0: 초기값
+    mu: 평균
+    sigma: 표준 편차
+    T: 시뮬레이션 시간
+    dt: 시간 간격
+    """
+
+    # Brownian motion
+    W = np.random.normal(0, 1, (T, 1))
+
+    daily_returns = tmp.pct_change().dropna()
+
+    # 연간 수익률
+    mean_return = daily_returns.mean()
+    annual_return =((1 + mean_return) ** T) - 1
+
+    # 변동성 계산
+    mu = annual_return/T
+    sigma = daily_returns.std()
+    
+    X = np.zeros((T, 1))
+    X[0] = S0
+    for t in range(1, T):
+        X[t] = X[t - 1] * np.exp((mu - sigma ** 2 / 2) * dt + sigma * W[t])
+
+    return X
+    
+def monte_sim(sim_num,tmp,stocks,stock_money,day=100):
+    sim_num = sim_num
+    balance_df = pd.DataFrame(np.zeros((sim_num,day)))
+    for i in range(len(stocks)):
+        X = []
+        for k in range(sim_num):
+            X.append(geometric_brownian_motion(tmp[stocks[i]],stock_money[stocks[i]].iloc[0]))
+        balance_df += pd.DataFrame(np.array(X).reshape(sim_num,day))
+    return balance_df.T
+
+
